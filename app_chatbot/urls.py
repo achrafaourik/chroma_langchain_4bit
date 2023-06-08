096866b131956e0c433c6883c3f4036d269e1938
Achraf Digital <achraf@digitalocean.com>
2023-06-08 17:57:56 +0000

    fix urls load model view

--- app_chatbot/urls.py
+++ app_chatbot/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ChatbotView.as_view(), name="nlp"),
-    path('load/', LoadModelsView.as_view(), name='huggingface')
+    path('load/', views.LoadModelsView.as_view(), name='huggingface')
 ]
