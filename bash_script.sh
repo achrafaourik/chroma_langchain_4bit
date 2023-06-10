a9bc2452fd309dbe59e92eeaf827a58061bf2819
Achraf <aaourik@pca.ma>
2023-06-10 20:34:32 +0100

    update database server ip

--- bash_script.sh
+++ bash_script.sh
@@ -8,9 +8,9 @@ export SQL_ENGINE="django.db.backends.postgresql"
 export SQL_DATABASE="django_rasa_prod"
 export SQL_USER="postgres_rasa"
 export SQL_PASSWORD="1234"
-export SQL_HOST="165.22.84.221"
+export SQL_HOST="165.227.170.110"
 export SQL_PORT="5432"
-export CHROMA_SERVER_HOST="165.22.84.221"
+export CHROMA_SERVER_HOST="165.227.170.110"
 export N_RELATED_INTERACTIONS="5"
 
 
@@ -29,7 +29,7 @@ pip install -r requirements_prod.txt
 echo "Starting makemigrations"
 python3 manage.py flush --no-input
 python3 manage.py makemigrations
-python3 manage.py collectstatic --no-input
+# python3 manage.py collectstatic --no-input
 echo "Finished makemigrations"
 
 echo "Starting migrate"
