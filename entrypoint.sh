046e917d1480b754b16c5e4b12e7e17b6feef459
Achraf <aaourik@pca.ma>
2023-06-12 22:10:18 +0100

    add env variables to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
+export DEBIAN_FRONTEND=noninteractive
+export SQL_ENGINE="django.db.backends.postgresql"
+export SQL_DATABASE="django_rasa_prod"
+export SQL_USER="postgres_rasa"
+export SQL_PASSWORD="1234"
+export SQL_HOST="165.227.170.110"
+export SQL_PORT="5432"
+export CHROMA_SERVER_HOST="165.227.170.110"
+export N_RELATED_INTERACTIONS="5"
 
 echo "Starting makemigrations"
 python3 manage.py flush --no-input
